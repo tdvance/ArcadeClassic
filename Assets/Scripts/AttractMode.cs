8dd79991fcec4bf16ae2440968f2afffa036fda5
tdvance <tdvance@gmail.com>
2016-12-23 12:18:44 -0500

    Initial Commit

---
+++ Assets/Scripts/AttractMode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractMode : MonoBehaviour {
+    FlexibleMusicManager fmm;
+	// Use this for initialization
+	void Start () {
+        fmm = FindObjectOfType<FlexibleMusicManager>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (fmm && fmm.CurrentTrackNumber() > 0) {
+            fmm.repeat = true;
+        }
+	}
+}
